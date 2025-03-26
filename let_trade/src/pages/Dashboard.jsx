b7b891d20538f83d5a715c167063c65d394a9379
Meku-nint <154579159+Meku-nint@users.noreply.github.com>
2025-03-26 20:20:58 +0300

    Update Dashboard.jsx

--- let_trade/src/pages/Dashboard.jsx
+++ let_trade/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
     const fetchOrders = async () => {
       const token = localStorage.getItem('token');
       try {
-        const res = await axios.get("http://localhost:3000/abc/getOrders", {
+        const res = await axios.get("https://devfastservice.onrender.com/abc/getOrders", {
           headers: {
             Authorization: `Bearer ${token}`,
           },    
